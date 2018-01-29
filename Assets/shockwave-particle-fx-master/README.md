5af2eb6ea73396f255ee9953b47be6d88c4e19ed
Vidhart Bhatia <vidhartbhatia@hotmail.com>
2018-01-29 18:53:34 -0500

    upload game files

---
+++ Assets/shockwave-particle-fx-master/README.md
@@ -0,0 +1,7 @@
+# shockwave-particle-fx
+shockwave particle effect 
+
+**Preview**
+[preview](http://i.imgur.com/VhyRIBi.gif)
+
+![Alt Text](https://media.giphy.com/media/l378yM3iaLOeNbDTq/giphy.gif)
---
+++ Assets/testparticle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testparticle : MonoBehaviour {
+	float radius = 10.0f;
+	// Use this for initialization
+	void Start () {
+		
+
+		ParticleSystem ps = GetComponent<ParticleSystem>();
+		var sh = ps.shape;
+		sh.radius = radius;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
+
+
